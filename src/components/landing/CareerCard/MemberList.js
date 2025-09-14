00420489ee34629cbf6f477be95859450f9f69cd
Oakslab-1018 <2857982860@qq.com>
2025-09-14 20:00:51 +0800

    feat: 添加CareerCard及一些样式

---
+++ src/components/landing/CareerCard/MemberList.js
@@ -0,0 +1,50 @@
+const careerList = [];
+const studentList = [];
+
+studentList.push(
+  { name: '李源', grade: '2015级', college: '成都信息工程大学' },
+  { name: '钟弋辰', grade: '2018级', college: '东华大学' },
+  { name: '张琳莉', grade: '2019级', college: '四川大学' },
+  { name: '李丹妮', grade: '2020级', college: '杭州电子科技大学' },
+  { name: '甘艾平', grade: '2020级', college: '四川大学' },
+  { name: '李天然', grade: '2020级', college: '西南交通大学' },
+  { name: '李思涵', grade: '2020级', college: '河北大学' },
+  { name: '王丽洪', grade: '2021级', college: '电子科技大学' },
+  { name: '董晋胜', grade: '2021级', college: '电子科技大学' }
+);
+
+careerList.push(
+  { name: '周杨', grade: '2017级', company: '青云科技' },
+  { name: '曾志伟', grade: '2017级', company: '青云科技' },
+  { name: '王宇', grade: '2018级', company: '美团' },
+  { name: '许龑澜', grade: '2018级', company: '阿里巴巴' },
+  { name: '李明杰', grade: '2018级', company: '浙江大华技术股份有限公司' },
+  { name: '吴旭东', grade: '2018级', company: '美团' },
+  { name: '徐佩熠', grade: '2018级', company: '腾讯' },
+  { name: '唐阳', grade: '2018级', company: '字节跳动' },
+  { name: '陶恒', grade: '2019级', company: '宝尊' },
+  { name: '陶伟杰', grade: '2019级', company: '广西电网' },
+  { name: '王璇', grade: '2019级', company: '深圳万兴科技有限公司' },
+  { name: '曹文', grade: '2019级', company: '成都六分鱼网络有限公司' },
+  { name: '熊康伟', grade: '2020级', company: '腾讯' },
+  { name: '王浩亭', grade: '2020级', company: '滴滴出行' },
+  { name: '丁杨维', grade: '2020级', company: '贝壳找房(北京)科技有限公司' },
+  { name: '张治华', grade: '2020级', company: '成都翼创雷行' },
+  { name: '桂威', grade: '2020级', company: '成都白之树' },
+  { name: '彭沥', grade: '2020级', company: '倍发科技有限公司' },
+  { name: '彭芳洁', grade: '2020级', company: '小黑盒' },
+  { name: '刘萌源', grade: '2020级', company: '交通银行股份有限公司' },
+  { name: '冯小强', grade: '2020级', company: '贝壳找房(北京)科技有限公司' },
+  { name: '刘涵铭', grade: '2020级', company: '字节跳动' },
+  { name: '陈雨', grade: '2020级', company: '中冶赛迪数字科技(成都)有限公司' },
+  { name: '卢一鹤', grade: '2021级', company: '浙江零跑科技股份有限公司' },
+  { name: '罗彧', grade: '2022级', company: '美团' },
+  { name: '周思儒', grade: '2022级', company: '字节跳动' },
+  { name: '黄礼森', grade: '2022级', company: '字节跳动' },
+  { name: '陈涛', grade: '2022级', company: '腾讯云计算(西安)责任有限公司' },
+  { name: '李传奇', grade: '2022级', company: '腾讯云计算(西安)责任有限公司' },
+  { name: '刘昊', grade: '2022级', company: '腾讯云计算(西安)责任有限公司' },
+  { name: '任欢', grade: '2023级', company: '美团' },
+  { name: '王雨萌', grade: '2023级', company: '安恒信息' }
+);
+export { careerList, studentList };
